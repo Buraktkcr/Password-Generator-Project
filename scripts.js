e010a4e377bcac9fc19b8d8a45a21d2bc5176b64
Buraktkcr <burak.tikicieri@ogr.gelisim.edu.tr>
2026-09-24 11:25:56 +0300

    commit project files

---
+++ scripts.js
@@ -0,0 +1,27 @@
+const characters = [
+    "A", "B", "C", "D", "E", "F", "G", "H", "I", "J",
+    "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T",
+    "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g",
+    "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t",
+    "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
+    "!", "@", "#", "$", "%", "^", "&", "*", "(", ")",
+    "-", "_", "=", "+", "[", "]", "{", "}", ";", ":",
+    ",", ".", "?", "/"
+];
+
+
+let firstPassword = document.getElementById("first-pswrd");
+let secondPassword = document.getElementById("second-pswrd");
+
+function generatePassword() {
+    firstPassword.textContent = "";
+    secondPassword.textContent = " ";
+
+    for (let i = 0; i < 15; i++) {
+        let randomCharacter = Math.floor(Math.random() * characters.length)
+        firstPassword.textContent += characters[randomCharacter];
+        randomCharacter = Math.floor(Math.random() * characters.length)
+        secondPassword.textContent += characters[randomCharacter];
+    }
+
+}
